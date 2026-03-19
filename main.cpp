30116ffd4be273a33ed5ffd48b8e8ec5d3f8a46f
Dharmande <compteslohe@protonmail.com>
2026-03-19 08:56:45 +0000

    code joueur en cours

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#ifndef comptead_h
+#define comptead_h 
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+
+class joueur : public compte{
+
+
+protected :
+enumCouleur Couleur;
+map<enumCouleur,int>;
+int mainWagon;
+vector<Ticket*> mainTicket;
+int nb_ticket_gagne;
+
+public:
+
+void pioche(int nb_a_piocher, pioche type);
+void poserWagon(enumVille a, enumVille b, enumCouleur c)
+void afficherMain();
+};
+
+#endif /* comptead_h */
