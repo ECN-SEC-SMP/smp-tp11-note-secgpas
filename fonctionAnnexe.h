534fb06f1cd89417bf11d3ca0406c3c2a3ea71fd
riana <cascade.lebehy@gmail.com>
2026-03-19 11:53:32 +0100

    voieFerree-ecrite-notest

---
+++ fonctionAnnexe.h
@@ -0,0 +1,11 @@
+#ifndef SMP_TP8_DAVID_RIANA_ANNEXE_H
+#define SMP_TP8_DAVID_RIANA_ANNEXE_H
+
+#include <iostream>
+#include <array>
+using namespace std; 
+
+array<string,2> calibrageVoieFerree();
+
+
+#endif
---
+++ src/VoieFerree.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<array>
+
+#include "VoieFerree.h"
+using namespace std;
+
+VoieFerree::VoieFerree(Ville_e v1, Ville_e v2,int p,Couleur_e couleur){
+    ville1 = v1;
+    ville2 = v2;
+    proprio = nullptr;
+    poids = p;
+    c = couleur;
+}
+
+Joueur* VoieFerree::getProprio(){
+    return proprio;
+}
+
+int VoieFerree::getPoids(){
+    return poids;
+}
+
+Couleur_e VoieFerree::getCouleur(){
+    return c;
+}       
+
+bool estDispo(){
+    return (proprio == nullptr);
+}
+
