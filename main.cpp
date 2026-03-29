fbb83634c4a819b1b49e135f759343092933c99c
ABABABgfg <yshotito@gmail.com>
2026-03-29 16:13:27 +0200

    main

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "Jeu.hpp"
+
+using namespace std;
+
+int main() {
+    int nbJoueurs;
+    cout << "Bienvenue dans les Aventuriers du Rail !" << endl;
+    cout << "Combien de joueurs ? (2-4) : ";
+    cin >> nbJoueurs;
+
+    while (nbJoueurs < 2 || nbJoueurs > 4) {
+        cout << "Nombre de joueurs invalide, réessaie (2-4) : ";
+        cin >> nbJoueurs;
+    }
+
+    Jeu jeu(nbJoueurs);
+    jeu.partie();
+    jeu.afficherEtat();
+
+    return 0;
+}
