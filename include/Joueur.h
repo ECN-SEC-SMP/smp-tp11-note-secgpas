3d5041bbb164f485990e1dfa3005f0b3218f57c8
ABABABgfg <yshotito@gmail.com>
2026-03-29 15:48:38 +0200

    Joueur_test

--- include/Joueur.h
+++ include/Joueur.h
@@ -1,32 +1,40 @@
-#ifndef SMP_TP8_DAVID_RIANA_VOIEFERREE_H
-#define SMP_TP8_DAVID_RIANA_VOIEFERREE_H
+#ifndef JOUEUR_H
+#define JOUEUR_H
 
 #include <iostream>
 #include <map>
 #include <vector>
-#include "fonctionAnnexe.h"
 #include "types.h"
 #include "Carte.h"
 #include "Pioche.h"
 #include "Ville.h"
 
+class Plateau; // forward declaration suffit ici
+
 using namespace std;
 
-class Joueur{
-    private : 
+class Joueur {
+    private:
         Couleur_e couleur;
-        map<Couleur_e,int> mainCarte;
+        map<Couleur_e, int> mainCarte;
         vector<Ticket*> mainTicket;
         int mainWagon;
-    public :
-        Joueur(Couleur_e);
-        int getMainWagon();
-        map<Couleur_e,int> getMainCarte();
-        void piocher(Pioche& type);
-        void poserWagon(Ville* a, Ville* b, Couleur_e c);
-        void defausser();
-        void afficherMain();
-        Ticket rendreVisible();
+
+    public:
+        Joueur(Couleur_e c);
+
+        // Getteurs
+        int getMainWagon() const;
+        Couleur_e getCouleur() const;
+        map<Couleur_e, int> getMainCarte() const;
+        vector<Ticket*> getMainTicket() const;
+
+        // Méthodes principales
+        void piocher(int nbAPiocher, Pioche& type);
+        void poserWagon(Ville_e a, Ville_e b, Couleur_e c, Plateau& plateau);
+        void defausser(Pioche& piocheTicket);       // votre version originale
+        void defausserCarte(Carte* c);              // défausse une carte précise
+        void afficherMain() const;
 };
 
 #endif
--- src/Joueur.cpp
+++ src/Joueur.cpp
@@ -21,46 +21,47 @@ map<Couleur_e,int> Joueur::getMainCarte(){
     return mainCarte;
 }
 
-void Joueur::piocher(Pioche& type){
+void Joueur::piocher(int nbAPiocher, Pioche& type){
     if (! type.estPiocheVide()){
         switch (type.getPiocheType())
     {
     case Pioche_type_e::CarteW :
-        for (int i = 0; i < 2; i++){
-            CTrain* carte = dynamic_cast<CTrain*>(type.getDeck().back()); //polymorphisme sur Carte et CTrain
-            type.getDeck().pop_back(); //retire la carte de la pioche
-            mainCarte[carte->getCouleur()] ++ ; //ajoute 1 au nombre de carte possédées par le joueur
+        for (int i = 0; i < nbAPiocher; i++){
+            CTrain* carte = dynamic_cast<CTrain*>(type.getDeck().back());
+            type.getDeck().pop_back();
+            mainCarte[carte->getCouleur()]++;
         }
         break;
 
     case Pioche_type_e::Ticket :
-        for (int i = 0; i < 2; i++){
-            Ticket* carte1 = dynamic_cast<Ticket*>(type.getDeck().back()); //polymorphisme sur Carte et Ticket
-            type.getDeck().pop_back(); //retire la carte de la pioche
+        for (int i = 0; i < nbAPiocher; i++){
+            Ticket* carte1 = dynamic_cast<Ticket*>(type.getDeck().back());
+            type.getDeck().pop_back();
             mainTicket.push_back(carte1);
         }
+        break;
+
     default:
         break;
     }
     }
-    
 }
 
-void Joueur::poserWagon(Ville* a, Ville* b, Couleur_e c){
-    for (int i = 0; i < /*nomplateau*/.getVoieFerree().size(); i++){
-        if (((/*nomplateau*/.getVoieFerree()[i].getVille1() == a && /*nomplateau*/.getVoieFerree()[i].getVille2() == b) ||(/*nomplateau*/.getVoieFerree()[i].getVille1() == b && /*nomplateau*/.getVoieFerree()[i].getVille2() == a)) && (/*nomplateau*/.getVoieFerree()[i].getCouleur() == c)){
-            if (/*nomplateau*/.getVoieFerree()[i].estDispo()){
-                mainWagon = mainWagon - /*nomplateau*/.getVoieFerree()[i].getPoids();
-                /*nomplateau*/.getVoieFerree()[i].setProprio(&this);
+void Joueur::poserWagon(Ville* a, Ville* b, Couleur_e c, Plateau& plateau){
+    for (int i = 0; i < plateau.getVoieFerree().size(); i++){
+        if (((plateau.getVoieFerree()[i].getVille1() == a && plateau.getVoieFerree()[i].getVille2() == b) ||(plateau.getVoieFerree()[i].getVille1() == b && plateau.getVoieFerree()[i].getVille2() == a)) && (plateau.getVoieFerree()[i].getCouleur() == c)){
+            if (plateau.getVoieFerree()[i].estDispo()){
+                mainWagon = mainWagon - plateau.getVoieFerree()[i].getPoids();
+                plateau.getVoieFerree()[i].setProprio(this); // &this -> this
             }
         }
     }
 }
 
-void Joueur::defausser(){
+void Joueur::defausser(Pioche& piocheTicket){
     mainTicket.pop_back();
     mainTicket.pop_back();
-    piocher(/*pioche qui stocke le deck ticket*/ );
+    piocher(2, piocheTicket);
 }
 
 void Joueur::afficherMain(){
