ad2287774ed11ccea77b5b9542685450202c878b
Vincent Arimon <vincent.arimon@gmail.com>
2026-04-12 16:49:18 +0200

    affiche regles

--- main.cpp
+++ main.cpp
@@ -7,6 +7,15 @@ using namespace std;
 int main() {
     int nbJoueurs = 0;
     cout << "Bienvenue dans les Aventuriers du Rail !" << endl;
+    cout << "\n--- Règles rapides (récapitulatif) ---" << endl;
+    cout << "- Chaque joueur commence avec 20 wagons." << endl;
+    cout << "- Mélanger les cartes Train et distribuer 4 cartes à chaque joueur (main cachée)." << endl;
+    cout << "- Former la pioche face cachée avec les cartes restantes." << endl;
+    cout << "- Mélanger les Tickets et distribuer 2 tickets à chaque joueur (tickets cachés)." << endl;
+    cout << "- Les cartes et tickets des joueurs restent secrets." << endl;
+    cout << "- Au tour d'un joueur, choisir : 0 Piocher | 1 Poser wagon | 2 Passer (défausse tickets)" << endl;
+    cout << "- Vous pouvez afficher le plateau ou votre main via les options prévues." << endl;
+    cout << "--------------------------------------\n" << endl;
 
     // Demande et validation robuste du nombre de joueurs (entier entre 2 et 4)
     while (true) {
